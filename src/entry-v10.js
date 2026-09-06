e4d76a2088afa074552855a2840c6c98eda031ca
Lixoy1 <lixoy11@gmail.com>
2026-09-07 00:06:58 +0300

    Use clean production admin console entry

--- src/entry-v10.js
+++ src/entry-v10.js
@@ -2,7 +2,7 @@ import "./runtime-v10.js";
 
 const admin = new URLSearchParams(window.location.search).get("admin") === "1";
 if(admin){
-  import("./admin-v10.jsx");
+  import("./admin-v11.jsx");
 }else{
   import("./main-v10.jsx");
 }
