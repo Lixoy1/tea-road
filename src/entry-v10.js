f5485da26db9f7bd837483cfeb4827735078382a
Lixoy1 <lixoy11@gmail.com>
2026-09-06 23:50:37 +0300

    Add Tea Road v1.0 app entry router

---
+++ src/entry-v10.js
@@ -0,0 +1,8 @@
+import "./runtime-v10.js";
+
+const admin = new URLSearchParams(window.location.search).get("admin") === "1";
+if(admin){
+  import("./admin-v10.jsx");
+}else{
+  import("./main-v10.jsx");
+}
